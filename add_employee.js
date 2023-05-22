e5c616395465e57d19a0c64b3b86628181290809
Monika Marek <62980345+monikamarr@users.noreply.github.com>
2023-05-21 20:31:23 -0700

    Add files via upload

---
+++ add_employee.js
@@ -0,0 +1,97 @@
+// Get the objects we need to modify
+let addEmployeeForm = document.getElementById('add-employee-form-ajax');
+
+// Modify the objects we need
+addEmployeeForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputFirstName = document.getElementById("input-firstName");
+    let inputLastName = document.getElementById("input-lastName");
+    let inputEmail = document.getElementById("input-email");
+    let inputPhoneNumber = document.getElementById("input-phoneNumber");
+
+    // Get the values from the form fields
+    let firstNameValue = inputFirstName.value;
+    let lastNameValue = inputLastName.value;
+    let emailValue = inputEmail.value;
+    let phoneNumberValue = inputPhoneNumber.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+        email: emailValue,
+        phoneNumber: phoneNumberValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-employee-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputFirstName.value = '';
+            inputLastName.value = '';
+            inputEmail.value = '';
+            inputPhoneNumber.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("employees-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let idEmployeeCell = document.createElement("TD");
+    let firstNameCell = document.createElement("TD");
+    let lastNameCell = document.createElement("TD");
+    let emailCell = document.createElement("TD");
+    let phoneNumberCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    idEmployeeCell.innerText = newRow.idEmployee;
+    firstNameCell.innerText = newRow.firstName;
+    lastNameCell.innerText = newRow.lastName;
+    emailCell.innerText = newRow.email;
+    phoneNumberCell.innerText = newRow.phoneNumber;
+
+    // Add the cells to the row 
+    row.appendChild(idEmployeeCell);
+    row.appendChild(firstNameCell);
+    row.appendChild(lastNameCell);
+    row.appendChild(emailCell);
+    row.appendChild(phoneNumberCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row);
+}
---
+++ app.js
@@ -0,0 +1,99 @@
+/*
+    SETUP
+*/
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+PORT        = 23254;                 // Set a port number at the top so it's easy to change in the future
+// Database
+var db = require('./database/db-connector')
+
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+/*
+    ROUTES
+*/
+// app.js
+
+app.get('/', function(req, res)
+    {  
+        let query1 = "SELECT * FROM Employees;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query
+
+/*
+POST
+*/
+
+// app.js
+
+
+app.post('/add-employee-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    // let homeworld = parseInt(data.homeworld);
+    // if (isNaN(homeworld))
+    // {
+    //     homeworld = 'NULL'
+    // }
+
+    // let age = parseInt(data.age);
+    // if (isNaN(age))
+    // {
+    //     age = 'NULL'
+    // }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Employees (firstName, lastName, email, phoneNumber) VALUES ('${data.firstName}', '${data.lastName}', ${data.email}, ${data.phoneNumber})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Employees;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
+    /*
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
